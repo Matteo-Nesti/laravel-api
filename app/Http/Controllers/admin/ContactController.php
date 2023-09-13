68a57f4a15f303f37f07322112bd63f674be6442
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-09-13 18:20:41 +0200

    fix data key

--- app/Http/Controllers/admin/ContactController.php
+++ app/Http/Controllers/admin/ContactController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Mail\ContactMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class ContactController extends Controller
 {
@@ -13,10 +14,27 @@ class ContactController extends Controller
 {
 $data = $request->all();
 
- $mail = new ContactMail(
+$validator = Validator::make($data, [
+    'email' => 'required|email',
+    'subject' => 'required|string',
+    'message' => 'required|string',
+],[
+    'email.required' => 'email is required',
+    'email.email' => 'email is not valid',
+    'subject.required' => 'subject is required',
+    'subject.string' => 'subject is not valid',
+    'message.required' => 'message is required',
+    'message.string' => 'message is not valid',
+]);
+
+if($validator->fails()){ // se fallisce la vaLIDazione rispondendo con un booleano
+    return response()->json( ['errors' => $validator->errors()], 400);  //mandare bad request, con il json rispondiamo con gli errori, come array associativo.
+}
+
+ $mail = new ContactMail(  //si istanzia la mail passandogli poi i data provenienti dalle request
     sender: $data['email'],
     subject: $data['subject'],
-    content: $data['content'],
+    content: $data['message'],
  );
  Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
 
