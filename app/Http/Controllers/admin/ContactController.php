1777300412341f74dae38854b1b34bd751144607
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-09-13 16:57:59 +0200

    make route, controller and mailable

---
+++ app/Http/Controllers/admin/ContactController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Mail\ContactMail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class ContactController extends Controller
+{
+    public function message()
+{
+ $mail = new ContactMail();
+ Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+ return response(null, 204);
+}
+}
